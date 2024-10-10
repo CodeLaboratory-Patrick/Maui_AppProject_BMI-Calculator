ec7066840ffec3ac169c6d0dc0f6ec2e505967ec
Donghwi_Park <gptnc5100@gmail.com>
2024-10-10 09:15:58 +0200

    Add project files.

---
+++ Maui_Project_BMI-Calculator/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui_Project_BMI_Calculator
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Maui_Project_BMI-Calculator/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Maui_Project_BMI_Calculator
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
