6e7f32c54b9da33d48eaf5ce2890903152bcc653
Donghwi_Park <gptnc5100@gmail.com>
2024-10-10 10:00:45 +0200

    Creation of sections for weight and height by using Syncfusion.Maui Nugetpackage.

--- Maui_Project_BMI-Calculator/MauiProgram.cs
+++ Maui_Project_BMI-Calculator/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Syncfusion.Maui.Core.Hosting;
 
 namespace Maui_Project_BMI_Calculator
 {
@@ -7,8 +8,10 @@ namespace Maui_Project_BMI_Calculator
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
+            
             builder
                 .UseMauiApp<App>()
+                .ConfigureSyncfusionCore()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
